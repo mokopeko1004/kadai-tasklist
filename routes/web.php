530a292e5db15401d12bdbd1b433dbca5ae80edd
masaki.nakamura <mokopeko1216@gmail.com>
2021-07-24 08:49:53 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,36 @@
-@extends('layouts.app')
+<?php
 
-@section('content')
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>やあ</h1>
-                {{-- 会員登録ページへのリンク --}}
-            </div>
-        </div>
-    @endif
-@endsection
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+//Route::get('/', 'MicropostsController@index');    // 上書き
+
+//Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+//Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// 認証
+//Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+//Route::post('login', 'Auth\LoginController@login')->name('login.post');
+//Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+//Route::group(['middleware' => ['auth']], function () {
+//    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+//    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+//});
